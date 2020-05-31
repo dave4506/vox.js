262e0a0ad2dd7fdcd886530cad59d448e40a06d8
David Sun <me@bydavidsun.com>
2020-05-31 12:48:04 -0700

    add THREE for node use

--- build/vox.js
+++ build/vox.js
@@ -1,5 +1,5 @@
 /*
- * vox.js 1.1.0-beta
+ * vox.js 1.1.0
  * https://github.com/daishihmr/vox.js
  * 
  * The MIT License (MIT)
@@ -497,6 +497,8 @@ var vox = {};
     };
 
 })();
+
+var THREE = require('three');
 
 (function() {
 
