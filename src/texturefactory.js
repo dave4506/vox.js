3c86a6dee0be4a83e47c09e5627dbd69119c7aa5
David Sun <me@bydavidsun.com>
2020-05-31 12:58:21 -0700

    disabled document use

--- src/texturefactory.js
+++ src/texturefactory.js
@@ -9,17 +9,18 @@
      * @return {HTMLCanvasElement}
      */
     vox.TextureFactory.prototype.createCanvas = function(voxelData) {
-        var canvas = document.createElement("canvas");
-        canvas.width = 256;
-        canvas.height= 1;
-        var context = canvas.getContext("2d");
-        for (var i = 0, len = voxelData.palette.length; i < len; i++) {
-            var p = voxelData.palette[i];
-            context.fillStyle = "rgb(" + p.r + "," + p.g + "," + p.b + ")";
-            context.fillRect(i * 1, 0, 1, 1);
-        }
+        throw new Error('createCanvas not supported in node.js')
+        // var canvas = document.createElement("canvas");
+        // canvas.width = 256;
+        // canvas.height= 1;
+        // var context = canvas.getContext("2d");
+        // for (var i = 0, len = voxelData.palette.length; i < len; i++) {
+        //     var p = voxelData.palette[i];
+        //     context.fillStyle = "rgb(" + p.r + "," + p.g + "," + p.b + ")";
+        //     context.fillRect(i * 1, 0, 1, 1);
+        // }
         
-        return canvas;
+        // return canvas;
     };
     
     /**
